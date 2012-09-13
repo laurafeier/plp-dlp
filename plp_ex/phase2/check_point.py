afa092922c0231a6fef6aac7bf291fb63559fce0
Laura Feier <laura.feier@threepillarglobal.com>
2012-09-13 11:39:53 +0300

    attr of classes changet to private

--- plp_ex/phase2/check_point.py
+++ plp_ex/phase2/check_point.py
@@ -42,42 +42,48 @@ class Card:
     TYPE = {0: "spade", 1: "heart", 2: "diamond", 3: "club"}
     
     def __init__(self, type_card, number):
-        self.type_card = type_card
-        self.number = number 
+        self.__type_card = type_card
+        self.__number = number 
     
     def str_pretty_print(self):
-        return self.type_card + str(self.number)
+        return self.__type_card + str(self.__number)
+    
+    def getnumber(self):
+        return self.__number
+    
+    def gettype_card(self):
+        return self.__type_card
     
 class CardDealer:
     """ card dealer """
     def __init__(self):
         print "building deck..."
-        self.cards = []
+        self.__cards = []
         for i in range(1,14):
             for key in Card.TYPE:
-                self.cards.append(Card(Card.TYPE[key],i))        
+                self.__cards.append(Card(Card.TYPE[key],i))        
         print self.str_pretty_print_cards()
         print "deck built"
         
     def shuffle_cards(self):
         print "shuffle cards"
-        shuffle(self.cards)
+        shuffle(self.__cards)
 
     def sort_cards(self):
         print "sort cards"
-        self.cards = sorted(self.cards, key=attrgetter("type_card","number"))
+        self.__cards = sorted(self.__cards, key=attrgetter("gettype_card","getnumber"))
     
     def send_cards(self, no_of_cards):
         print "sending "+ str(no_of_cards) + " cards from deck"
         cards_to_send = []
         for i in range(no_of_cards):
-            cards_to_send.append(self.cards.pop())
+            cards_to_send.append(self.__cards.pop())
         print "sending " + str(self.str_pretty_print_cards(cards_to_send))
         return cards_to_send
         
     def str_pretty_print_cards(self, arr_of_cards = None):
         if arr_of_cards is None:
-            return [card.str_pretty_print() for card in self.cards]
+            return [card.str_pretty_print() for card in self.__cards]
         return [card.str_pretty_print() for card in arr_of_cards]
         
 class Player:
@@ -87,7 +93,7 @@ class Player:
     
     def receive_cards(self, cards):
         print "player " + str(self.id) + " receives cards"
-        self.cards = cards
+        self.__cards = cards
     
     def getid(self):
         return self.id
@@ -95,30 +101,27 @@ class Player:
 class Game:
     """ abstract object used only to register players and dealer""" 
     def __init__(self, no_of_players):
-        self.dealer = CardDealer()
-        self.players = []
+        self.__dealer = CardDealer()
+        self.__players = []
         for el in range(no_of_players):
-            self.players.append(Player(el))
+            self.__players.append(Player(el))
     
     def start_game(self):
-        print "players" +  str([pl.id for pl in self.players])            
+        print "players" +  str([pl.id for pl in self.__players])            
 
-        self.dealer.sort_cards()
-        print self.dealer.str_pretty_print_cards()
-        self.dealer.shuffle_cards()
-        print self.dealer.str_pretty_print_cards()
+        self.__dealer.sort_cards()
+        print self.__dealer.str_pretty_print_cards()
+        self.__dealer.shuffle_cards()
+        print self.__dealer.str_pretty_print_cards()
         
         print "send cards to player"
-        for player in self.players:
-            player.receive_cards(self.dealer.send_cards(2))
+        for player in self.__players:
+            player.receive_cards(self.__dealer.send_cards(2))
         
         print "draw the table hand of 5 cards"
-        self.dealer.send_cards(5)
+        self.__dealer.send_cards(5)
         
-        print "cards left in deck: " + str(self.dealer.str_pretty_print_cards())
-    
-    def getplayers(self):
-        return self.players
+        print "cards left in deck: " + str(self.__dealer.str_pretty_print_cards())
             
 game = Game(3)
 game.start_game()
