e394fb6a02b7fb285127a524952cada80cb85b05
Laura Feier <laura.feier@threepillarglobal.com>
2012-09-14 15:35:39 +0300

    pb 3 done ... one more to go

--- plp_ex/phase3/check_point.py
+++ plp_ex/phase3/check_point.py
@@ -6,6 +6,7 @@ Created on Sep 13, 2012
 #*******************************
 #P1
 #*******************************
+from _pyio import __metaclass__
 
 tree = ("b", ("a", None, None), ("z", ("c", None, None), ("zz", None, None)))
 
@@ -32,4 +33,25 @@ print [ set(el) for r in range(len(data)+1) for el in combinations(data, r) ]
 #*******************************
 #P3
 #*******************************
+class   Singleton( type ):
+    _all_instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._all_instances:
+            print "create instance for ", cls
+            cls._all_instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._all_instances[cls]
+    
+class X:
+    __metaclass__ = Singleton
+    def __init__(self, i):
+        self.i = i
 
+class Y:
+    __metaclass__ = Singleton
+    def __init__(self, i):
+        self.i = i
+
+print X(5).i  
+print X(2).i
+print Y(10).i  
+print Y(19).i
