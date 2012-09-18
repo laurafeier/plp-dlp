a4a3a93a43d632a9e42616b261c8cef997be67ce
Laura Feier <laura.feier@threepillarglobal.com>
2012-09-18 17:15:19 +0300

    phase 3 done

--- plp_ex/phase3/check_point.py
+++ plp_ex/phase3/check_point.py
@@ -7,6 +7,9 @@ Created on Sep 13, 2012
 #P1
 #*******************************
 from _pyio import __metaclass__
+import __main__
+import TYPES
+import inspect
 
 tree = ("b", ("a", None, None), ("z", ("c", None, None), ("zz", None, None)))
 
@@ -51,7 +54,83 @@ class Y:
     def __init__(self, i):
         self.i = i
 
-print X(5).i  
-print X(2).i
+x5 = X(5).i  
+x2 = X(2).i
+assert x5 is x2
+import ipdb; ipdb.set_trace()
 print Y(10).i  
 print Y(19).i
+
+
+#*******************************
+#P4
+#*******************************
+print "**********************"
+
+class DataType():
+    STRING =0
+    INT = 1
+
+class MyModel():
+    
+    def __init__(self, **kwargs):
+        
+        for key in kwargs:
+            if key in self.__class__.__dict__:
+                if self.__class__.__dict__[key] is DataType.STRING and type(kwargs[key]) is str:
+                    self.__dict__[key] = kwargs[key]
+                elif self.__class__.__dict__[key] is DataType.INT and type(kwargs[key]) is int:
+                    self.__dict__[key] = kwargs[key]
+                else:
+                    raise Exception("type conversion error")
+    
+    def save(self):
+        all_fields = [f for f in self.__class__.__dict__ if f not in ["__module__", "__doc__"]]
+        for f in all_fields:
+            if f not in self.__dict__:
+                raise Exception("field required to update db")
+        print "update table"
+        
+def model(**options):
+    def _model(function):
+        def inner():
+            model_name = function.__name__
+            data = function()
+            if options.has_key("name"):
+                model_name = options["name"]
+            print model_name
+            t = type(model_name, (MyModel, ), data)
+            globals().update({model_name: t})
+        model.all[function.__name__] = inner
+        
+        return model.all[function.__name__]
+    return _model
+model.all={}
+
+@model(name="Person")
+def some_function():
+    return {
+            "first_name":DataType.STRING, 
+            "last_name": DataType.STRING, 
+            "age": DataType.INT
+            }
+
+@model(name="Dude")
+def asdas():
+    return {
+            "first_name":DataType.STRING, 
+            "last_name": DataType.STRING, 
+            "age": DataType.INT
+            }
+#build all models
+print model.all
+for k,v in model.all.iteritems():
+    v()
+    
+
+print MyModel.__subclasses__()
+
+a = Person(first_name="name", last_name="last", age=20)
+# todo type conversion error on setters
+#a.last_name = 2
+a.save() 
