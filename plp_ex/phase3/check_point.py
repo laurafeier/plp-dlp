423aeabd0b6a3d4f771761ae193f926f8ff4f744
Laura Feier <laura.feier@threepillarglobal.com>
2012-09-19 15:15:26 +0300

    removed trailing spaces

--- plp_ex/phase3/check_point.py
+++ plp_ex/phase3/check_point.py
@@ -6,12 +6,9 @@ Created on Sep 13, 2012
 #*******************************
 #P1
 #*******************************
-from _pyio import __metaclass__
-import __main__
-import TYPES
-import inspect
 
 tree = ("b", ("a", None, None), ("z", ("c", None, None), ("zz", None, None)))
+
 
 def do(tree):
     yield tree[0]
@@ -21,7 +18,7 @@ def do(tree):
     if tree[2] is not None:
         for el in do(tree[2]):
             yield el
-            
+
 print [el for el in do(tree)]
 #*******************************
 #P2
@@ -31,34 +28,41 @@ data = set([1, 2, 3])
 
 from itertools import combinations
 
-print [ set(el) for r in range(len(data)+1) for el in combinations(data, r) ] 
+print [set(el) for r in range(len(data) + 1) for el in combinations(data, r)]
 
 #*******************************
 #P3
 #*******************************
-class   Singleton( type ):
+
+
+class Singleton(type):
     _all_instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._all_instances:
             print "create instance for ", cls
             cls._all_instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._all_instances[cls]
-    
+
+
 class X:
     __metaclass__ = Singleton
+
     def __init__(self, i):
         self.i = i
+
 
 class Y:
     __metaclass__ = Singleton
+
     def __init__(self, i):
         self.i = i
 
-x5 = X(5).i  
+x5 = X(5).i
 x2 = X(2).i
 assert x5 is x2
-import ipdb; ipdb.set_trace()
-print Y(10).i  
+
+print Y(10).i
 print Y(19).i
 
 
@@ -67,14 +71,16 @@ print Y(19).i
 #*******************************
 print "**********************"
 
+
 class DataType():
-    STRING =0
+    STRING = 0
     INT = 1
 
-class MyModel():
-    
+
+class MyModel(object):
+
     def __init__(self, **kwargs):
-        
+
         for key in kwargs:
             if key in self.__class__.__dict__:
                 if self.__class__.__dict__[key] is DataType.STRING and type(kwargs[key]) is str:
@@ -83,54 +89,55 @@ class MyModel():
                     self.__dict__[key] = kwargs[key]
                 else:
                     raise Exception("type conversion error")
-    
+
     def save(self):
         all_fields = [f for f in self.__class__.__dict__ if f not in ["__module__", "__doc__"]]
         for f in all_fields:
             if f not in self.__dict__:
                 raise Exception("field required to update db")
         print "update table"
-        
+
+
 def model(**options):
     def _model(function):
         def inner():
             model_name = function.__name__
             data = function()
-            if options.has_key("name"):
+            if "name" in options:
                 model_name = options["name"]
             print model_name
             t = type(model_name, (MyModel, ), data)
             globals().update({model_name: t})
         model.all[function.__name__] = inner
-        
+
         return model.all[function.__name__]
     return _model
-model.all={}
+model.all = {}
+
 
 @model(name="Person")
 def some_function():
     return {
-            "first_name":DataType.STRING, 
-            "last_name": DataType.STRING, 
-            "age": DataType.INT
-            }
+    "first_name": DataType.STRING,
+    "last_name": DataType.STRING,
+    "age": DataType.INT}
+
 
 @model(name="Dude")
 def asdas():
     return {
-            "first_name":DataType.STRING, 
-            "last_name": DataType.STRING, 
-            "age": DataType.INT
-            }
+    "first_name": DataType.STRING,
+    "last_name": DataType.STRING,
+    "age": DataType.INT
+    }
 #build all models
 print model.all
-for k,v in model.all.iteritems():
+for k, v in model.all.iteritems():
     v()
-    
 
 print MyModel.__subclasses__()
 
 a = Person(first_name="name", last_name="last", age=20)
 # todo type conversion error on setters
 #a.last_name = 2
-a.save() 
+a.save()
